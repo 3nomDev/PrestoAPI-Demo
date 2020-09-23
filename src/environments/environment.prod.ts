41a66f8e86d40255777c851d454405b4a405428f
asamole <49318174+asamole@users.noreply.github.com>
2020-09-23 11:20:12 -0400

    gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  googleClientId:
-    '526157046335-teuntd4q2f6uuhcm1gpqrki3lcq84q33.apps.googleusercontent.com',
-  fbAppId: '2412174265691357',
-};
